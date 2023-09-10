357f884ec01ced19a17e13b772046afdff66dfdf
ERROR 404: NULL NOT FOUND <giggabyte6@gmail.com>
2023-09-09 21:34:25 -0500

    I have a bad habit of constantly pushing before I check

--- build.sh
+++ build.sh
@@ -28,42 +28,42 @@ printf "OK\n"
 
 printf "[[ Dependancies satisfied, building ]]\n"
 
-if [ -d "./build" ]; then
+if [ -d "build" ]; then
   printf "Detected existing build directory\n"
   printf "=> Deleting build directory..."
-  rm -r ./build 
+  rm -r build 
   printf "OK\n"
 fi
 printf "=> Creating build directory..."
-mkdir -p ./build/modules/style
+mkdir -p build/modules/style
 printf "OK\n"
 
-printf "=> Minifying src/index.html (outputting to ./build/index.html)..."
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true src/index.html > ./build/index.html
+printf "=> Minifying src/index.html (outputting to build/index.html)..."
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true src/index.html > build/index.html
 printf "OK\n"
 printf "  OG Size: $(du --apparent-size -shc src/index.html | head -1)\n"
 printf "  Minified size: $(du --apparent-size -shc build/index.html | head -1)\n"
 
-printf "=> Minifying src/main.js (outputting to ./build/main.js)..."
-uglifyjs --compress -- src/main.js > ./build/main.js
+printf "=> Minifying src/main.js (outputting to build/main.js)..."
+uglifyjs --compress -- src/main.js > build/main.js
 printf "OK\n"
 printf "  OG Size: $(du --apparent-size -shc src/main.js | head -1)\n"
 printf "  Minifid size: $(du --apparent-size -shc build/main.js | head -1)\n"
 
-printf "=> Minifying src/emojis.json (outputting to ./build/emojis.json)..."
-uglifyjs --compress --expression -- src/emojis.json > ./build/emojis.json
+printf "=> Minifying src/emojis.json (outputting to build/emojis.json)..."
+uglifyjs --compress --expression -- src/emojis.json > build/emojis.json
 printf "OK\n"
 printf "  OG Size: $(du --apparent-size -shc src/emojis.json | head -1)\n"
 printf "  Minified size: $(du --apparent-size -shc build/emojis.json | head -1)\n"
 
-printf "=> Minifying src/style.css (outputting to ./build/style.css)..."
-csso src/style.css > ./build/style.css
+printf "=> Minifying src/style.css (outputting to build/style.css)..."
+csso src/style.css > build/style.css
 printf "OK\n"
 printf "  OG Size: $(du --apparent-size -shc src/style.css | head -1)\n"
 printf "  Minified size: $(du --apparent-size -shc build/style.css | head -1)\n"
 
-printf "=> Minifying src/modules/style/error.css (outputting to ./build/modules/style/error.css)..."
-csso src/modules/style/error.css > ./build/modules/style/error.css
+printf "=> Minifying src/modules/style/error.css (outputting to build/modules/style/error.css)..."
+csso src/modules/style/error.css > build/modules/style/error.css
 printf "OK\n"
 printf "  OG Size: $(du --apparent-size -shc build/modules/style/error.css | head -1)\n"
 printf "  Minified size: $(du --apparent-size -shc build/modules/style/error.css | head -1)\n"
@@ -80,4 +80,4 @@ printf "OK\n"
 printf "  OG Size: $(du --apparent-size -shc src/modules/style/input.css | head -1)\n"
 printf "  Minified size: $(du --apparent-size -shc build/modules/style/input.css | head -1)\n"
 printf "[[ Build finished! ]]\n"
-printf "Total size of files: $(du --apparent-size -shc build | head -1 | sed "s/build//g")"
+printf "Total size of files: $(du --apparent-size -shc build | head -1 | sed "s/build//g")\n"
